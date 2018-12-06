e74f9e5e3eca06cf6dec5d0eccdef7dd24045f74
andy <weiainiandy@hotmail.com>
2018-12-06 22:43:28 +0800

    20181206

--- WChatApp/src/compos/TabBarIcon.js
+++ WChatApp/src/compos/TabBarIcon.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Platform,View,Image } from 'react-native';
+import { Platform,View,Image,StyleSheet } from 'react-native';
 
 export default class TabBarIcon extends React.Component {
     constructor(props) {
         super(props);
+        this.state={
+            img:props.img,
+            focusimg:props.focusimg
+        }
     }
 
     render(){
         return (
-            <View>
-                <Image source={this.props.focused?require('../assets/img/tabBarIcon/chat_fcous.png'):require('../assets/img/tabBarIcon/chat.png')}></Image>
+            <View style={styles.contais}>
+                <Image style={styles.icon} resizeMode="contain" source={this.props.focused?this.state.focusimg:this.state.img}></Image>
             </View>
         )
     }
-
 }
+
+const styles=StyleSheet.create({
+    contais:{
+        height:30
+    },
+    icon:{
+        width:30,
+        height:30
+    }
+})
